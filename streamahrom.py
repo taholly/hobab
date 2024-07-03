da8ee985cb643af9a2b628fa8e93a6eaec0b429a
taha sadeghzadeh <87468299+taholly@users.noreply.github.com>
2024-07-03 15:33:02 +0330

    Update streamahrom.py

--- streamahrom.py
+++ streamahrom.py
@@ -44,7 +44,7 @@ def make_hobab_plot():
     p.vbar(x=[i for i in range(len(x))], top=y, width=0.4, color="blue")
 
     p.xgrid.grid_line_color = None
-    p.y_range.start = min(df['hobab].min() , 0)
+    p.y_range.start = min(df['hobab'].min() , 0)
     p.xaxis.major_label_orientation = 1
    
 
