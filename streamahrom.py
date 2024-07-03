55726b106484525db49bfc36fba598c72be3416c
taha sadeghzadeh <87468299+taholly@users.noreply.github.com>
2024-07-03 12:51:43 +0330

    Update streamahrom.py

--- streamahrom.py
+++ streamahrom.py
@@ -3,6 +3,7 @@ import requests
 from io import BytesIO
 import streamlit as st
 from bokeh.plotting import figure
+from bokeh.models import LinearAxis, Range1d
 
 # بارگذاری داده‌ها از URL
 url = 'https://raw.githubusercontent.com/taholly/hobab/main/ahromi'
@@ -32,9 +33,17 @@ def make_plot():
                toolbar_location=None, tools="", width=600)
 
     # اضافه کردن میله‌های عمودی برای هر دو ستون
-    p.vbar(x=[i - 0.2 for i in range(len(x))], top=y1, width=0.4, color="blue", legend_label="Hobab")
-    p.vbar(x=[i + 0.2 for i in range(len(x))], top=y2, width=0.4, color="green", legend_label="Leverage")
+    p.vbar(x=[i - 0.2 for i in range(len(x))], top=y1, width=0.4, color="blue", legend_label="Hobab", y_range_name='y1')
+    p.vbar(x=[i + 0.2 for i in range(len(x))], top=y2, width=0.4, color="green", legend_label="Leverage", y_range_name='y2')
 
+    # تنظیم محور y اصلی
+    p.y_range = Range1d(start=0, end=max(y1.max(), y2.max()) * 1.1)
+    p.extra_y_ranges = {"y2": Range1d(start=0, end=max(y2) * 1.1)}
+    
+    # اضافه کردن محور y ثانویه
+    p.add_layout(LinearAxis(y_range_name='y2', axis_label="Leverage"), 'right')
+
+    # تنظیمات اضافی
     p.xgrid.grid_line_color = None
     p.y_range.start = 0
     p.xaxis.major_label_orientation = 1
