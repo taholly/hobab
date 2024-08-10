6dfab2827086ab7b28635e0398ccd200a340b619
taha sadeghzadeh <87468299+taholly@users.noreply.github.com>
2024-08-10 12:08:40 +0330

    Update streamahrom.py

--- streamahrom.py
+++ streamahrom.py
@@ -4,62 +4,31 @@ from io import BytesIO
 import streamlit as st
 import plotly.graph_objs as go
 
-async def hobab_tala():
-    
-    dictdf = {}
-    talafund = ["طلا" , "آلتون", "تابش","جواهر" ,"زر","زرفام","عیار","کهربا","گنج","گوهر","مثقال","ناب","نفیس","نفیس"]
-    for fund in talafund:
-        inst = await Instrument.from_search(fund)
-        live = await inst.live_data()
-        price = live['pl']
-        nav = live['nav']
-        time = live['nav_datetime']
-        dictdf[fund] = [fund ,price ,nav, time ] 
-        
-    df = pd.DataFrame(dictdf , index = ["nemad" ,'Price' , 'NAV', "Time"])
-    df = df.T.assign(hobab = (df.T["Price"] - df.T["NAV"])/ df.T["NAV"])
-    return df
-
-async def hobab_ahrom():
-    
-    dictdf = {}
-    talafund  = ["اهرم" , "توان" , "موج" , "نارنج اهرم" , "شتاب" , "جهش" , "بیدار"]
-    for fund in talafund:
-        inst = await Instrument.from_search(fund)
-        live = await inst.live_data()
-        price = live['pl']
-        nav = live['nav']
-        time = live['nav_datetime']
-        dictdf[fund] = [fund ,price ,nav, time ] 
-        
-    df = pd.DataFrame(dictdf , index = ["nemad" ,'Price' , 'NAV', "Time"])
-    df = df.T.assign(hobab = (df.T["Price"] - df.T["NAV"])/ df.T["NAV"])
-    return df
-
-async def hobab_ETF():
-    
-    dictdf = {}
-    talafund  = ETF=["آتیمس","آساس","تاراز","آوا","ارزش","نارین","افق ملت","الماس","پیروز","انار","اوج","بازبیمه","بهین رو","پتروآبان","پتروداریوش","پتروصبا","پتروما","سمان","پتروآگاه","متال","رویین","تخت گاز","استیل","فلزفارابی","آذرین","بذر","پادا","پالایش","پرتو","کاردان","ترمه","اطلس","ثروتم","ثمین","داریوش","ثهام","هامون","هیوا","جاودان","برلیان","دریا","رماس","زرین","ثنا","سرو","سلام","آبنوس","ویستا","اکسیژن","بیدار","توان","جهش","شتاب","اهرم","موج","نارنج اهرم","سپینود","تیام","ثروت ساز","کاریس","هوشیار","فیروزه","آرام","وبازار","صدف","فراز","فارما کیان","درسا","هم وزن","خلیج","مدیر","مروارید","تکپاد","عقیق","آگاس","دارا یکم"]
-    for fund in talafund:
-        inst = await Instrument.from_search(fund)
-        live = await inst.live_data()
-        price = live['pl']
-        nav = live['nav']
-        time = live['nav_datetime']
-        dictdf[fund] = [fund ,price ,nav, time ] 
-        
-    df = pd.DataFrame(dictdf , index = ["nemad" ,'Price' , 'NAV', "Time"])
-    df = df.T.assign(hobab = (df.T["Price"] - df.T["NAV"])/ df.T["NAV"])
-    return df
-
-async def load_data(option):
-    if option == "ETF":
-        df =await hobab_ETF()
+# بارگذاری داده‌ها از URL
+def load_data(option):
+    if option == "طلا":
+        file_name = "tala.xlsx"
     elif option == "اهرم":
-        df =await hobab_ahrom()
+        file_name = "ahromi"
     else:
-        df =await hobabItala()
-    return df
+        file_name = "ETF.xlsx"
+
+    url = f'https://raw.githubusercontent.com/taholly/hobab/main/{file_name}'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        file = BytesIO(response.content)
+        try:
+            df = pd.read_excel(file, engine='openpyxl')
+            df = df.rename(columns={'Unnamed: 0': "nemad"})
+            return df
+        except Exception as e:
+            st.error(f"Error reading the Excel file: {e}")
+            return None
+    else:
+        st.error(f"Failed to retrieve file: {response.status_code}")
+        return None
+
 
 # ایجاد نمودار حباب
 def create_hobab_plot(df):
@@ -103,19 +72,19 @@ st.title(f"محاسبه ی حباب صندوق های {option}")
 
 df = await load_data(option)
 
-df2 = df.iloc[:,1:]
+#df2 = df.iloc[:,1:]
 if df2 is not None:
-    df2 = df2.round(3)
-    st.write(df2)
+    df = df.round(3)
+    st.write(df)
 
     # نمایش نمودار حباب
     hobab_plot = create_hobab_plot(df)
     st.plotly_chart(hobab_plot)
 
     # نمایش نمودار اهرم و پراکندگی در صورت انتخاب گزینه 'اهرم'
-    #if option == "اهرم":
-        #leverage_plot = create_leverage_plot(df)
-        #st.plotly_chart(leverage_plot)
+    if option == "اهرم":
+        leverage_plot = create_leverage_plot(df)
+        st.plotly_chart(leverage_plot)
 
 
 
